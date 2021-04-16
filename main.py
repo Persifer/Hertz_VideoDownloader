53165b8934b5bca9f93de9ec4fad37927bd269c2
Persifer <giorgino.antonio32@gmail.com>
2021-04-16 21:44:04 +0200

    commit due per rebase

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import videoDownloadHandler as downloader
 import startingPage as home
 
 if __name__ == "__main__":
-    # path = str(input("Insersci la cartella di destinazione del file \n-->"))
-    # url = str(input("Insersci il path del video da scaricare \n-->"))
-    # downloader.downloadVideoByUrl(url, path)
-
     home.main_window()
+
+#TODO
+# 1) Creare le finestre di dialogo per avvertire che il download è finito
+# 2) Trasformare il videodownloader da funzioni a classi
