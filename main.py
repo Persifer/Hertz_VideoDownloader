6be547c00d3b190c5f78dda882a39a9d3c08cc25
Persifer <giorgino.antonio32@gmail.com>
2021-04-30 19:16:37 +0200

    Modifica requirements.txt file

--- main.py
+++ main.py
@@ -4,5 +4,6 @@ if __name__ == "__main__":
     home.main_window()
 
 #TODO
-# 1) Creare le finestre di dialogo per avvertire che il download è finito
-# 2) Trasformare il videodownloader da funzioni a classi
+# 1) Trasformare il videodownloader da funzioni a classi
+# 2) Risolvere il fatto che non sempre comudir
+# ncia la fine del download
